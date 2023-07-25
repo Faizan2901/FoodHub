d35dba75799893aec7e8415e75e1d1f2c27fd6aa
Faizan2901 <mo.faizansopariwala2901@gmail.com>
2023-07-25 18:39:43 +0530

    Optimize code

--- src/main/java/com/codemind/FoodHub/dao/FoodItemsDAO.java
+++ src/main/java/com/codemind/FoodHub/dao/FoodItemsDAO.java
@@ -13,9 +13,4 @@ public interface FoodItemsDAO extends JpaRepository<FoodItems,Integer> {
     @Query(value = "SELECT COUNT(*) as dp_count FROM food_items_user_list fu GROUP BY user_list_id,food_items_id HAVING fu.user_list_id=:userId and fu.food_items_id=:foodId",nativeQuery = true)
     int countOfSameItemsofCustomer(@Param("userId") int userId,@Param("foodId") int foodId);
 
-    @Query(value = "DELETE FROM food_items_user_list fu WHERE fu.user_list_id=:userId and fu.food_items_id=:foodId AND NOT IN(SELECT MIN(user_list_id),MIN(food_items_id) FROM fu GROUP BY user_list_id,food_items_id",nativeQuery = true)
-    void removeRedundantItemForCustomer(@Param("userId") int userId,@Param("foodId") int foodId);
-
-
-
 }
