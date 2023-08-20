eb2ca8a30a7a9e60968a9c97f73750936249be75
Faizan2901 <mo.faizansopariwala2901@gmail.com>
2023-08-20 22:29:23 +0530

    Optimize code

--- src/main/java/com/codemind/FoodHub/service/UserService.java
+++ src/main/java/com/codemind/FoodHub/service/UserService.java
@@ -3,7 +3,6 @@ package com.codemind.FoodHub.service;
 import com.codemind.FoodHub.entity.User;
 import com.codemind.FoodHub.user.WebUser;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.ui.Model;
 
 
 public interface UserService extends UserDetailsService {
